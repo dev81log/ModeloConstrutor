8733bfb2a72502b0e25aaf61743f817533077e5b
dev81log <dev81log@gmail.com>
2022-07-09 22:09:12 -0300

    teste

--- ContentContext/Estudante.cs
+++ ContentContext/Estudante.cs
@@ -5,16 +5,18 @@ namespace ModeloConstrutor.ContentContext
     public class Estudante
     {
 
-        public Estudante(DateTime mensalidade, string nome, string email, EFormacaoCursos formacao)
+        public Estudante(DateTime mensalidade, bool mensalidadePaga, string nome, string email, EFormacaoCursos formacao)
         {
             IdEstudante = Guid.NewGuid();
-            Mensalidade = mensalidade;
+            MensalidadePaga = IsPa
             Nome = nome;
             Email = email;
             Formacao = formacao;
         }
 
+
         public Guid IdEstudante { get; set; }
+        public bool MensalidadePaga { get; set; } = false;
         public DateTime Mensalidade { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
