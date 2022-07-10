a5428d7b97055b025f3f1256c5a99c8d0a1d54cf
dev81log <dev81log@gmail.com>
2022-07-09 22:10:11 -0300

    subir

--- ContentContext/Pagamento.cs
+++ ContentContext/Pagamento.cs
@@ -7,18 +7,18 @@ namespace ModeloConstrutor.ContentContext
             IdPagamento = idPagamento;
             IdCurso = idCurso;
             Valor = valor;
-
         }
 
         public int IdPagamento { get; set; }
         public DateTime DataPagamento => new DateTime(2022, 2, 2);
         public int IdCurso { get; set; }
         public decimal Valor { get; set; }
+        public bool Pago => true;
 
         // Método que retorna se o pagamento foi realizado ou não que é passado pelo estudante
         public bool IsPago(DateTime mensalidade)
         {
-            return DataPagamento.Date == mensalidade.Date;
+            return DataPagamento.Date <= mensalidade.Date;
         }
 
 
